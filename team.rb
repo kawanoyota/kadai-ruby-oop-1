9c0b078623f5ebce547f891a1fb0fce1d884c827
yota <heyetech01@gmail.com>
2021-11-12 10:22:28 +0000

    修正

--- team.rb
+++ team.rb
@@ -9,26 +9,26 @@ class Team
     self.name = name
   end
  
-  def calc_win_rate(name)
+  def calc_win_rate
     self.win / (self.win.to_f + self.lose)
   end
-  
-  def show_team_result(name)
-    puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{calc_win_rate(name)}です。"
+
+  def show_team_result
+    puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{calc_win_rate}です。"
   end
 
 end
 
-Giants = Team.new(67,45,87,"Giants")
-Tigers = Team.new(60,53,7,"Tigers")
-Dragons = Team.new(60,55,5,"Dragons")
-BayStars = Team.new(56,58,12,"BayStars")
-Carp = Team.new(52,56,12,"Carp")
-Swallows = Team.new(41,69,10,"Swallows")
+giants = Team.new(67,45,87,"Giants")
+tigers = Team.new(60,53,7,"Tigers")
+dragons = Team.new(60,55,5,"Dragons")
+bayStars = Team.new(56,58,12,"BayStars")
+carp = Team.new(52,56,12,"Carp")
+swallows = Team.new(41,69,10,"Swallows")
 
-Giants.show_team_result(Giants)
-Tigers.show_team_result(Tigers)
-Dragons.show_team_result(Dragons)
-BayStars.show_team_result(BayStars)
-Carp.show_team_result(Carp)
-Swallows.show_team_result(Swallows)
+giants.show_team_result
+tigers.show_team_result
+dragons.show_team_result
+bayStars.show_team_result
+carp.show_team_result
+swallows.show_team_result
